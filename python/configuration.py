628c319e623f4eb2aacd8773d39c017f206f72ca
VictoriaRaul <Vikaraul1289@mail.ru>
2024-07-08 17:25:11 +0300

    Дипломная работа

---
+++ python/configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://43da086c-e773-4a1c-a97a-eaf36fc90cf6.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders/"
+GET_ORDER_PATH = "/api/v1/orders/track/"
---
+++ python/data.py
@@ -0,0 +1,15 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Рома",
+    "lastName": "Зверев",
+    "address": "Хорошая улица 2",
+    "metroStation": 5,
+    "phone": "+74416329777",
+    "rentTime": 2,
+    "deliveryDate": "2024-07-15",
+    "comment": "Буду ждать",
+    "color": ["BLACK"]
+}
